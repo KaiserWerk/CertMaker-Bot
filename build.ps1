e4cf22d899119b43c2f5a63b6c73bee5fcb16c65
Robin Kaiser <web@r-k.mx>
2021-05-18 19:03:55 +0200

    updating the build file

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$sourcecode = "."
+$sourcecode = "cmd/certmaker-bot/main.go"
 $target = "build/certmaker-bot"
 # Windows, 64-bit
 $env:GOOS = 'windows'; $env:GOARCH = 'amd64';             go build -o "$($target)-win64.exe" -ldflags "-s -w" $sourcecode
