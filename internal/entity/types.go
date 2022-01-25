9c9933586307f6eb3b25c30f88c3a9cc82e5b2dd
Robin Kaiser <web@r-k.mx>
2022-01-25 12:51:20 +0100

    Update types.go

--- internal/entity/types.go
+++ internal/entity/types.go
@@ -1,13 +1,5 @@
 package entity
 
-type Configuration struct {
-	CertMaker struct {
-		Host       string `yaml:"host"`
-		SkipVerify bool   `yaml:"skip_verify"`
-		ApiKey     string `yaml:"apikey"`
-	} `yaml:"certmaker"`
-}
-
 type CertificateRequirement struct {
 	Domains []string `yaml:"domains"`
 	IPs     []string `yaml:"ips"`
@@ -19,8 +11,8 @@ type CertificateRequirement struct {
 		StreetAddress string `yaml:"street_address"`
 		PostalCode    string `yaml:"postal_code"`
 	} `yaml:"subject,omitempty"`
-	Days     int    `yaml:"days"`
-	CertFile string `yaml:"cert_file"`
-	KeyFile  string `yaml:"key_file"`
+	Days         int      `yaml:"days"`
+	CertFile     string   `yaml:"cert_file"`
+	KeyFile      string   `yaml:"key_file"`
 	PostCommands []string `yaml:"post_commands,omitempty"`
 }
