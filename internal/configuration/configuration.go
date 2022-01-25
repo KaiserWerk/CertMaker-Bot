a64c9148049473bd376e9ba0029bfeba25f28e27
Robin Kaiser <web@r-k.mx>
2022-01-25 12:51:30 +0100

    improved the configuration

--- internal/configuration/configuration.go
+++ internal/configuration/configuration.go
@@ -2,57 +2,51 @@ package configuration
 
 import (
 	"github.com/KaiserWerk/CertMaker-Bot/internal/assets"
-	"github.com/KaiserWerk/CertMaker-Bot/internal/entity"
 	helper "github.com/KaiserWerk/CertMaker-Bot/internal/helper"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"time"
 )
 
-var configFile = "config.yaml"
-
-func SetConfigurationFile(f string) {
-	configFile = f
+type AppConfig struct {
+	App struct {
+		MinValidity time.Duration `yaml:"min_validity"`
+		Interval    time.Duration `yaml:"interval"`
+	} `yaml:"app"`
+	CertMaker struct {
+		Host       string `yaml:"host"`
+		SkipVerify bool   `yaml:"skip_verify"`
+		ApiKey     string `yaml:"apikey"`
+	} `yaml:"certmaker"`
 }
 
-func GetConfiguration() (*entity.Configuration, error) {
-	cont, err := ioutil.ReadFile(configFile)
-	if err != nil {
-		return nil, err
+func Setup(file string) (*AppConfig, bool, error) {
+	var created bool
+	if !helper.FileExists(file) {
+		if err := os.MkdirAll(filepath.Dir(file), 0700); err != nil {
+			return nil, false, err
+		}
+
+		cont, err := assets.GetConfigAsset("config.dist.yaml")
+		if err != nil {
+			return nil, false, err
+		}
+
+		if err := ioutil.WriteFile(file, cont, 0700); err != nil {
+			return nil, false, err
+		}
+		created = true
 	}
 
-	var c entity.Configuration
+	cont, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, created, err
+	}
+
+	var c AppConfig
 	err = yaml.Unmarshal(cont, &c)
-	return &c, err
-}
 
-func SetupConfiguration() (*entity.Configuration, error) {
-	if !helper.FileExists(configFile) {
-		configPath := filepath.Dir(configFile)
-		_ = os.MkdirAll(configPath, 0755)
-
-		configAssets := assets.GetConfigurationAssets()
-		cont, err := configAssets.ReadFile("config/config.dist.yaml")
-		if err != nil {
-			return nil, err
-		}
-
-		fh, err := os.Create(configFile)
-		if err != nil {
-			return nil, err
-		}
-
-		_, err = fh.Write(cont)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	c, err := GetConfiguration()
-	if err != nil {
-		return nil, err
-	}
-
-	return c, nil
+	return &c, created, nil
 }
