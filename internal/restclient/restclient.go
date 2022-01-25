f9e92d5be6477a19f53e5db275d5b1401736ce95
Robin Kaiser <web@r-k.mx>
2022-01-25 12:51:08 +0100

    reworked the restclient

--- internal/restclient/restclient.go
+++ internal/restclient/restclient.go
@@ -2,69 +2,43 @@ package restclient
 
 import (
 	"crypto/tls"
-	"fmt"
-	"github.com/KaiserWerk/CertMaker-Bot/internal/configuration"
 	"net/http"
 	"net/url"
 	"time"
 )
 
-var (
-	client     = http.Client{Timeout: 3 * time.Second}
-	caHost     = "http://localhost:8880"
-	skipVerify = false
-)
-
-func GetClient() *http.Client {
-	return &client
+type RestClient struct {
+	client *http.Client
+	caHost string
+	apiKey string
+	config *tls.Config
 }
 
-func SetCaHost(h string, skip bool) {
-	caHost = h
-	skipVerify = skip
-}
-
-func GetTlsConfig() *tls.Config {
-	config := &tls.Config{
-		// CipherSuites not needed with TLS 1.3
-		//CipherSuites: []uint16{
-		//	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-		//	tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-		//},
-		MinVersion:               tls.VersionTLS13,
-		PreferServerCipherSuites: true,
-		InsecureSkipVerify:       skipVerify,
+func New(caHost, apiKey string, skipVerify bool) *RestClient {
+	return &RestClient{
+		client: &http.Client{
+			Timeout: 3 * time.Second,
+			Transport: &http.Transport{TLSClientConfig: &tls.Config{
+				MinVersion:               tls.VersionTLS13,
+				PreferServerCipherSuites: true,
+				InsecureSkipVerify:       skipVerify,
+			}},
+		},
+		caHost: caHost,
+		apiKey: apiKey,
 	}
-
-	return config
 }
 
-func ExecuteRequest(r *http.Request) (*http.Response, error) {
-	cl := GetClient()
-
-	hostUrl, err := url.ParseRequestURI(caHost)
+func (rc *RestClient) ExecuteRequest(r *http.Request) (*http.Response, error) {
+	hostUrl, err := url.ParseRequestURI(rc.caHost)
 	if err != nil {
-		return nil, fmt.Errorf("could not parse CA host '%s': %s", caHost, err.Error())
-	}
-
-	if hostUrl.Scheme == "https" {
-		cl.Transport = &http.Transport{TLSClientConfig: GetTlsConfig()}
+		return nil, err
 	}
 
 	r.URL.Host = hostUrl.Host
 	r.URL.Scheme = hostUrl.Scheme
 
-	config, err := configuration.GetConfiguration()
-	if err != nil {
-		return nil, err
-	}
+	r.Header.Set("X-Auth-Token", rc.apiKey)
 
-	r.Header.Set("X-Auth-Token", config.CertMaker.ApiKey)
-
-	resp, err := cl.Do(r)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
+	return rc.client.Do(r)
 }
